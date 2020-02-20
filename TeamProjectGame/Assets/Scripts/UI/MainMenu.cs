a80665c14b895872bec737ea27b58a551a436ea0
Josh Douglas <100476935@unimail.derby.ac.uk>
2020-02-20 11:26:07 +0000

    menu added

---
+++ TeamProjectGame/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        print("Loading Scene: Level_1");
+        SceneManager.LoadScene("Level_1");
+    }
+}
